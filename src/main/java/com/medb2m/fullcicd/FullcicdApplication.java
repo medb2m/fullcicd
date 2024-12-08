8e19f1a724ca39dac6874bae0e1a02233ad76f4e
medb2m <79202818+medb2m@users.noreply.github.com>
2024-12-08 21:33:20 +0100

    first commit

---
+++ src/main/java/com/medb2m/fullcicd/FullcicdApplication.java
@@ -0,0 +1,13 @@
+package com.medb2m.fullcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullcicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullcicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medb2m/fullcicd/FullcicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medb2m.fullcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullcicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
